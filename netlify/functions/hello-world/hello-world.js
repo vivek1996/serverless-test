10bda2b208d5b5d74901228737c91203c412a9b1
Vivek <mvivek3112@gmail.com>
2022-07-09 14:10:49 +0530

    added parser

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -2,23 +2,39 @@
 
 const { Telegraf } = require("telegraf");
 const fetch = require("node-fetch");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 const handler = async (event) => {
   try {
     const tweets = await fetch(
-      "https://api.twitter.com/2/tweets/search/recent?query=from%3A965845248281755649",
+      "https://api.twitter.com/2/tweets/search/recent?query=from%3A36327407&tweet.fields=attachments,author_id,created_at,geo,lang,source,text&expansions=attachments.media_keys,author_id&media.fields=preview_image_url,url",
       {
         headers: { Authorization: `Bearer ${process.env.TWITTER_TOKEN}` },
       }
     );
-    console.log(tweets);
     const tweetsJson = await tweets.json();
-    await bot.telegram.sendMessage("-1001518787587", JSON.stringify(tweetsJson));
+    console.log(tweetsJson);
+    const { data } = tweetsJson;
+    const html = data
+      .map((tweet) => {
+        const date = new Date(tweet.created_at);
+        return `
+         Tweeted at - <b>${date.toLocaleString("en-IN")} </b>
+         Tweet:
+          <pre>${tweet.text}</pre>
+      `;
+      })
+      .join("\n");
+    await bot.telegram.sendMessage("-1001518787587", html, {
+      parse_mode: "HTML",
+    });
+    // bot.telegram.
     return {
       statusCode: 200,
-      body: 'Done',
+      body: "Done",
     };
   } catch (error) {
     await bot.telegram.sendMessage("-1001518787587", JSON.stringify(error));
